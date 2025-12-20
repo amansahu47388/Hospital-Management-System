2305cd15df1b94a16044e788c2a755a8d7f2b620
Aman <amansahu47388@gmail.com>
2025-12-20 15:25:48 +0530

    OPD/IPD model

--- Backend/Hospital_Project/doctor_module/models.py
+++ Backend/Hospital_Project/doctor_module/models.py
@@ -7,3 +7,15 @@ class Department(models.Model):
     def __str__(self):
         return self.name
 
+class Doctor(models.Model):
+    user = models.OneToOneField('users.User', on_delete=models.CASCADE)
+    department = models.ForeignKey(Department, on_delete=models.SET_NULL, null=True)
+    specialization = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=15, blank=True)
+    address = models.TextField(blank=True)
+    profile_picture = models.ImageField(upload_to="doctor_profiles/", blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Dr. {self.user.first_name} {self.user.last_name}"
--- Backend/Hospital_Project/opd_ipd_module/models.py
+++ Backend/Hospital_Project/opd_ipd_module/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+class OpdPatient(models.Model):
+    patient = models.ForeignKey('patient_module.Patient', on_delete=models.CASCADE)
+    appointment = models.ForeignKey('appointment_module.Appointment', on_delete=models.CASCADE)
+    consutant_doctor = models.ForeignKey('doctor_module.Doctor', on_delete=models.CASCADE)
+    opd_no = models.CharField(max_length=20, unique=True)
+    case_id = models.CharField(max_length=20, unique=True)
+    old_patient = models.BooleanField(default=False)
+    visit_date = models.DateTimeField(auto_now_add=True)
+    previos_medical_issue = models.TextField(blank=True, null=True)
+    created_by = models.ForeignKey('users.User', on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"OPD Visit - {self.patient.first_name} on {self.visit_date.strftime('%Y-%m-%d')}"
+
+
+
+class IpdPatient(models.Model):
+    patient = models.ForeignKey('patient_module.Patient', on_delete=models.CASCADE)
+    admission_date = models.DateTimeField(auto_now_add=True)
+    discharge_date = models.DateTimeField(null=True, blank=True)
+    ipd_no = models.CharField(max_length=20, unique=True)
+    case_id = models.CharField(max_length=20, unique=True)
+    old_patient = models.BooleanField(default=False)
+    previous_medical_issue = models.TextField(blank=True, null=True)
+    created_by = models.ForeignKey('users.User', on_delete=models.SET_NULL, null=True)
+    consutant_doctor = models.ForeignKey('doctor_module.Doctor', on_delete=models.CASCADE)
+    bad = models.ForeignKey('setup_module.Bad', on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"IPD Visit - {self.patient.first_name} on {self.admission_date.strftime('%Y-%m-%d')}"
+
