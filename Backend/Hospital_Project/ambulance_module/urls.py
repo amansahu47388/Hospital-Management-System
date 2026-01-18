9d5fb8dbed1cb8ee639e3e93df2e71299b924392
Monukushwah-aids <mk7289173@gmail.com>
2026-01-18 17:02:53 +0530

    add the backend birthdeath

--- Backend/Hospital_Project/ambulance_module/urls.py
+++ Backend/Hospital_Project/ambulance_module/urls.py
@@ -6,7 +6,7 @@ from .views import (
     AmbulanceCreateAPIView,
     AmbulanceUpdateAPIView,
     AmbulanceDeleteAPIView,
-
+    AmbulanceDetailAPIView,
     
 
     # Bills
@@ -14,7 +14,12 @@ from .views import (
     AmbulanceBillDetailAPIView,
     GenerateAmbulanceBillAPIView,
     AmbulanceBillUpdateView,
+    AmbulanceBillDeleteView,
     
+    # Transactions
+    AmbulanceBillTransactionListAPIView,
+    AmbulanceBillTransactionCreateAPIView,
+    AmbulanceBillTransactionDeleteAPIView,
 )
 
 urlpatterns = [
@@ -23,11 +28,17 @@ urlpatterns = [
     path('ambulance/create/', AmbulanceCreateAPIView.as_view()),
     path('ambulance/<int:pk>/update/', AmbulanceUpdateAPIView.as_view()),
     path('ambulance/<int:pk>/delete/', AmbulanceDeleteAPIView.as_view()),
+    path('ambulance/<int:pk>/', AmbulanceDetailAPIView.as_view()),
 
     # Bills
     path('ambulance-bill/', AmbulanceBillListAPIView.as_view()),
     path('ambulance-bill/<int:pk>/', AmbulanceBillDetailAPIView.as_view()),
     path('ambulance-bill/create/', GenerateAmbulanceBillAPIView.as_view()),
     path("ambulance-bill/<int:pk>/update/", AmbulanceBillUpdateView.as_view()),
+    path("ambulance-bill/<int:pk>/delete/", AmbulanceBillDeleteView.as_view()),
     
+    # Transactions
+    path('ambulance-bill/<int:bill_id>/transactions/', AmbulanceBillTransactionListAPIView.as_view()),
+    path('ambulance-bill/<int:bill_id>/transactions/create/', AmbulanceBillTransactionCreateAPIView.as_view()),
+    path('ambulance-bill/<int:bill_id>/transactions/<int:transaction_id>/delete/', AmbulanceBillTransactionDeleteAPIView.as_view()),
 ]
